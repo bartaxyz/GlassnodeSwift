197df7ffde8defbb805356faf0463e568b94b3a2
Ondřej Bárta <email@ondrejbarta.xyz>
2023-11-22 21:40:24 +0100

    Initial Commit

---
+++ glassnode-swift/glassnode_swift.h
@@ -0,0 +1,18 @@
+//
+//  glassnode_swift.h
+//  glassnode-swift
+//
+//  Created by Ondrej Barta on 22.11.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for glassnode_swift.
+FOUNDATION_EXPORT double glassnode_swiftVersionNumber;
+
+//! Project version string for glassnode_swift.
+FOUNDATION_EXPORT const unsigned char glassnode_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <glassnode_swift/PublicHeader.h>
+
+
